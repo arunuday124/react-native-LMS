f3c51997c33299bcf98d4678a660e34e6596a357
Arunuday <debnath.arunuday007@gmail.com>
2025-03-29 13:42:33 +0530

    update

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/components/Login';
+import Signup from './src/components/Signup';
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer >
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
